c3b0482e8f3698fc95ec0a3b4cf7b1804cdf8101
Kristiyan Pelteshki <kpelt@softserveinc.com>
2019-01-22 14:33:03 +0200

    Updated snakeGame

--- snake-game/snake.js
+++ snake-game/snake.js
@@ -1,7 +1,7 @@
 let key = document.getElementById('snake');
 const speed = 2;
-let w = document.body.getBoundingClientRect().width;
-let h = document.body.getBoundingClientRect().height;
+let w = document.body.offsetWidth;
+let h = document.body.offsetHeight;
 
 const directions = {
     39: { item: 'left', sign: 1 }, //right
@@ -25,15 +25,19 @@ function moveSnake() {
 
     if (parseInt(key.style.left) < 0) {
         key.style.left = w + 'px';
+        key.scrollIntoView({block: "center"});
     }
     else if (parseInt(key.style.top) < 0) {
         key.style.top = h + 'px';
+        key.scrollIntoView({block: "center"});
     }
     else if (parseInt(key.style.left) + key.style.width > w && direction.sign === 1) {
         key.style.left = 0;
+        key.scrollIntoView({block: "center"});
     }
     else if (parseInt(key.style.top) + key.style.height > h && direction.sign === 1) {
         key.style.top = 0;
+        key.scrollIntoView({block: "center"});
     }
     key.style[direction.item] = (parseInt(key.style[direction.item]) + direction.sign * speed) + 'px';
     window.requestAnimationFrame(moveSnake);
