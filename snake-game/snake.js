7ccdf100c2f949fb9240a4bf04ab1df807bdb270
MihailZhelev <mzhele@softserveinc.com>
2019-01-22 13:49:02 +0200

    Mihail - adding moving snake code

---
+++ snake-game/snake.js
@@ -0,0 +1,41 @@
+let key = document.getElementById('snake');
+const speed = 2;
+let w = document.body.getBoundingClientRect().width;
+let h = document.body.getBoundingClientRect().height;
+
+const directions = {
+    39: { item: 'left', sign: 1 }, //right
+    37: { item: 'left', sign: -1 }, //left
+    40: { item: 'top', sign: 1 }, //top
+    38: { item: 'top', sign: -1 }, //bottom
+}
+let direction = directions['39'];
+key.style.left = '0px';
+key.style.top = '0px';
+
+
+document.addEventListener('keydown', changeDirection);
+
+function changeDirection(event) {
+    if (Object.keys(directions).indexOf(String(event.keyCode)) != -1) {
+        direction = directions[event.keyCode];
+    }
+}
+function moveSnake() {
+
+    if (parseInt(key.style.left) < 0) {
+        key.style.left = w + 'px';
+    }
+    else if (parseInt(key.style.top) < 0) {
+        key.style.top = h + 'px';
+    }
+    else if (parseInt(key.style.left) + key.style.width > w && direction.sign === 1) {
+        key.style.left = 0;
+    }
+    else if (parseInt(key.style.top) + key.style.height > h && direction.sign === 1) {
+        key.style.top = 0;
+    }
+    key.style[direction.item] = (parseInt(key.style[direction.item]) + direction.sign * speed) + 'px';
+    window.requestAnimationFrame(moveSnake);
+}
+moveSnake();
