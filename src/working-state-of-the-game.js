24acc8a09d537f1d4651ebe81f53a5023eea5a65
Kristian Angelski <kange@softserveinc.com>
2019-01-25 19:05:55 +0200

    adding snake movement

--- src/working-state-of-the-game.js
+++ src/working-state-of-the-game.js
@@ -1,201 +1,164 @@
-//global variables
-let snake = [];
-let w = document.body.offsetWidth;
-let h = document.body.scrollHeight;
-let direction = 'right';
-const snakeDotSize = 30;
-const speed = 2;
-const directions = {
-    'right': {
-        item: 'left',
-        sign: 1
-    }, //right
-    'left': {
-        item: 'left',
-        sign: -1
-    }, //left
-    'down': {
-        item: 'top',
-        sign: 1
-    }, //top
-    'top': {
-        item: 'top',
-        sign: -1
-    }, //bottom
-}
-const keycodesToDirection = {
-    39: 'right',
-    37: 'left',
-    40: 'down',
-    38: 'top'
+const gameConstants = {
+	maxPageWidth: document.body.offsetWidth,
+	maxPageHeight: document.body.scrollHeight,
+	snakeBodySize: 20,
+	sizeDimension: 'px'
 };
-//create div to disable all background elements
-function createDiv() {
-    let div = document.createElement('div');
-    div.style.width = '100%';
-    div.style.height = '100%';
-    div.style.background = 'rgba(255, 255, 255, .4)';
-    div.style.color = 'white';
-    div.style.position = 'fixed';
-    div.style.top = '0';
-    div.style.opacity = '0.2';
-    div.setAttribute('id', 'Div1');
-    div.style.zIndex = '10000000000000';
-    document.body.id = 'body';
-    document.getElementById('body').appendChild(div);
+
+const directions = {
+	'right': {
+		item: 'right',
+		inputMapping: [39]
+	},
+	'left': {
+		item: 'left',
+		inputMapping: [37]
+	},
+	'down': {
+		item: 'bottom',
+		inputMapping: [40]
+	},
+	'top': {
+		item: 'top',
+		inputMapping: [38]
+	},
 }
 
-document.addEventListener('keydown', changeDirection);
+class Snake {
+	constructor(maxWidthUnits, maxHeightUnits) {
+		this.maxWidthUnits = maxWidthUnits;
+		this.maxHeightUnits = maxHeightUnits;
+		this.snakeBody = [];
+		this.snakeHeadPosition = {
+			x: 0,
+			y: 0,
+		};
+		this.snakeHeadDOM = createSnakeHead();
+		this.direction = directions.right;
+		this.addBody();
+		this.addBody();
+		this.addBody();
+	}
 
-function changeDirection(event) {
-    if (Object.keys(keycodesToDirection).indexOf(String(event.keyCode)) != -1) {
-        direction = keycodesToDirection[event.keyCode];
-        snake[0].direction = direction;
-        snake[0].oldDirection = direction;
-    }
+	move() {
+		let newSnakeHeadPosition = {
+			x: this.snakeHeadPosition.x,
+			y: this.snakeHeadPosition.y
+		};
+		switch (this.direction) {
+			case directions.right:
+				newSnakeHeadPosition.x = this.snakeHeadPosition.x + 1;
+				if (newSnakeHeadPosition.x > this.maxWidthUnits) {
+					newSnakeHeadPosition.x = 0;
+				}
+				break;
+			case directions.left:
+				newSnakeHeadPosition.x = this.snakeHeadPosition.x - 1;
+				if (newSnakeHeadPosition.x < 0) {
+					newSnakeHeadPosition.x = this.maxWidthUnits;
+				}
+				break;
+			case directions.down:
+				newSnakeHeadPosition.y = this.snakeHeadPosition.y + 1;
+				if (newSnakeHeadPosition.y > this.maxHeightUnits) {
+					newSnakeHeadPosition.y = 0;
+				}
+				break;
+			case directions.top:
+				newSnakeHeadPosition.y = this.snakeHeadPosition.y - 1;
+				if (newSnakeHeadPosition.y < 0) {
+					newSnakeHeadPosition.y = this.maxHeightUnits;
+				}
+				break;
+		}
+		let firstElement = this.snakeBody.shift();
+		let oldSnakePosition = this.snakeHeadPosition;
+		firstElement.x = oldSnakePosition.x;
+		firstElement.y = oldSnakePosition.y;
+		this.snakeBody.push(firstElement);
+
+		this.snakeHeadPosition = newSnakeHeadPosition;
+	}
+
+	getSnakeBody() {
+		return this.snakeBody.concat([{
+			x: this.snakeHeadPosition.x,
+			y: this.snakeHeadPosition.y,
+			DOMElement: this.snakeHeadDOM
+		}]);
+	}
+
+	changeDirection(direction) {
+		this.direction = direction;
+	}
+
+	addBody() {
+		this.snakeBody.push({
+			x: 0,
+			y: 0,
+			DOMElement: createSnakeBody()
+		});
+	}
 }
 
-function moveSnake() {
-
-    for (let i = 0; i < snake.length; i++) {
-        let snakeCurrentDot = document.getElementById(snake[i].id);
-        if (i + 1 < snake.length) {
-            let currDirection = directions[snake[i].direction];
-           // console.log(currDirection)
-           
-                snake[i + 1].previousY = snake[i].y + snakeDotSize;
-                snake[i + 1].previousX = snake[i].x
-                snake[i + 1].oldDirection = snake[i].direction;
-          
-                snake[i + 1].previousX = snake[i].x + snakeDotSize;
-                snake[i + 1].previousY = snake[i].y
-                snake[i + 1].oldDirection = snake[i].direction;
-      
-
-           
-        }
-
-        if (snake[i].direction !== snake[i].oldDirection) {
-            snake[i].x = snake[i].previousX;
-            snake[i].y = snake[i].previousY;
-            snake[i].direction = snake[i].oldDirection;
-        } else {
-            if (parseInt(snakeCurrentDot.style.left) < 0) {
-                snake[i].x = w - snakeDotSize;
-            } else if (parseInt(snakeCurrentDot.style.top) < 0) {
-                snake[i].y = h - snakeDotSize;
-            } else if (parseInt(snakeCurrentDot.style.left) + parseInt(snakeCurrentDot.style.width) > w) {
-                snake[i].x = 0
-            } else if (parseInt(snakeCurrentDot.style.top) + parseInt(snakeCurrentDot.style.height) > h) {
-                snake[i].y = 0;
-            }
-
-        }
-
-        // if(direction !== snake[i].oldDirection) {
-        //     debugger;
-        // }
-
-        let directionData = directions[snake[i].direction];
-        if (directionData.item === 'left') {
-            snake[i].x += directionData.sign * speed;
-        } else {
-            snake[i].y += directionData.sign * speed;
-        }
-
-        // snakeCurrentDot.previousX = snakeCurrentDot.style.top;
-        // snakeCurrentDot.previousY = snakeCurrentDot.style.left;
-
-        // if (i>0) {
-        //     let snakePrevousDot  = document.getElementById(snake[i-1].id);
-        //     snakeCurrentDot.pr
-        //     snakePrevousDot.style.top = snakeCurrentDot.previousX;
-        //     snakePrevousDot.style.left = snakeCurrentDot.previousY;
-        //     snakePrevousDot.direction = snakeCurrentDot.oldDirection;
-        // }
-
-        // snakeCurrentDot.previousX = snakePrevousDot.x
-        // snakeCurrentDot.previousX = snake
-        // if (i>0) {
-
-        //     snakePrevousDot.style.top = snakeCurrentDot.style.top;
-        //     snakePrevousDot.style.left = snakeCurrentDot.style.left;
-        //     snakePrevousDot.direction = snakeCurrentDot.oldDirection;
-        //     snakeCurrentDot.style.top = snakePrevousDot.style.top;
-        //     snakeCurrentDot.style.left = snake.snakePrevousDot.style.left;
-        // }
-
-
-
-        // if (snake[i].oldDirection !== 0) {
-        //     if (snake[i].direction !== snake[i].oldDirection) {
-        //         snake[i].direction === snake[i].oldDirection;
-        //     }
-        // }
-        snakeCurrentDot.style.left = snake[i].x + 'px';
-        snakeCurrentDot.style.top = snake[i].y + 'px';
-
-    }
-    document.getElementById(snake[0].id).scrollIntoView({
-        block: "center"
-    });
-    window.requestAnimationFrame(moveSnake);
-
+function draw(snake) {
+	let snakeBody = snake.getSnakeBody().reverse();
+	for (element of snakeBody) {
+		element.DOMElement.style.left = (element.x * gameConstants.snakeBodySize) + gameConstants.sizeDimension;
+		element.DOMElement.style.top = (element.y * gameConstants.snakeBodySize) + gameConstants.sizeDimension;
+	}
 }
 
-function makeSnakeItem() {
-    console.log(direction);
-    let x = 0;
-    let y = 0;
-    let previousItem = 0;
-    if (snake.length > 0) {
-        previousItem = snake[snake.length - 1];
-        x = previousItem.x;
-        y = previousItem.y;
-        switch (direction) {
-            case 'left':
-            case 'right':
-                x += (directions[direction].sign * -1) * snakeDotSize;
-                break;
-            case 'top':
-            case 'down':
-                y += (directions[direction].sign * -1) * snakeDotSize;
-                break;
-        }
-
-    }
-
-    let newDot = {
-        x,
-        y,
-        oldDirection: direction,
-        direction: direction,
-        previousX: 0,
-        previousY: 0,
-        id: 'snake' + snake.length
-    }
-    console.log(newDot);
-    //if direction is different check
-    makeDot(newDot);
-    snake.push(newDot);
+function createSnakeBody() {
+	let snakeItem = document.createElement('div');
+	snakeItem.style.width = gameConstants.snakeBodySize + gameConstants.sizeDimension;
+	snakeItem.style.height = gameConstants.snakeBodySize + gameConstants.sizeDimension;
+	snakeItem.style.position = 'fixed';
+	snakeItem.style.background = 'green';
+	snakeItem.style.zIndex = '9999999';
+	document.body.appendChild(snakeItem);
+	return snakeItem;
 }
 
-function makeDot(newDot) {
-    let snakeItem = document.createElement('div');
-    snakeItem.setAttribute('id', newDot.id);
-    snakeItem.style.borderRadius = '100px';
-    snakeItem.style.width = snakeDotSize + 'px';
-    snakeItem.style.height = snakeDotSize + 'px';
-    snakeItem.style.background = (snake.length === 0) ? 'black' : 'yellow';
-    snakeItem.style.position = 'absolute';
-    snakeItem.style.zIndex = '10000000000000';
-    snakeItem.style.top = newDot.y + 'px';
-    snakeItem.style.left = newDot.x + 'px';
-    document.body.appendChild(snakeItem);
+function createSnakeHead() {
+	let snakePart = createSnakeBody();
+	snakePart.style.background = 'black';
+	return snakePart;
 }
 
-$(document).ready(function () {
-    createDiv();
-    makeSnakeItem();
-    moveSnake();
+// //create div to disable all background elements
+// function createDiv() {
+//     let div = document.createElement('div');
+//     div.style.width = '100%';
+//     div.style.height = '100%';
+//     div.style.background = 'rgba(255, 255, 255, .4)';
+//     div.style.color = 'white';
+//     div.style.position = 'fixed';
+//     div.style.top = '0';
+//     div.style.opacity = '0.2';
+//     div.setAttribute('id', 'Div1');
+//     div.style.zIndex = '10000000000000';
+//     document.body.id = 'body';
+//     document.getElementById('body').appendChild(div);
+// }
+
+
+let maxWidthUnits = Math.floor(gameConstants.maxPageWidth / gameConstants.snakeBodySize);
+let maxHeightUnits = Math.floor(gameConstants.maxPageHeight / gameConstants.snakeBodySize);
+let snake = new Snake(maxWidthUnits, maxHeightUnits);
+
+document.addEventListener('keydown', (event) => {
+	let keyPressed = event.keyCode;
+	let direction = Object.keys(directions).forEach((key) => {
+		if (directions[key].inputMapping.indexOf(keyPressed) !== -1) {
+			// console.log(currentDirection);
+			snake.changeDirection(directions[key]);
+		}
+	});
 });
+
+
+setInterval(() => {
+	snake.move();
+	draw(snake);
+}, 100)
