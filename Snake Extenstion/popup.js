f2e0f199a9f305281a8d1d1ab958ba7342e5f521
Kristiyan Pelteshki <kpelt@softserveinc.com>
2019-01-31 17:14:16 +0200

    Made the extension remember the user's selected colours and percentage

--- Snake Extenstion/popup.js
+++ Snake Extenstion/popup.js
@@ -6,24 +6,31 @@ document.addEventListener('DOMContentLoaded', function () {
     var snakeBody = document.getElementById('body');
     var percentText = document.getElementById('percentText');
 
+    chrome.storage.sync.get(['snakeHeadColor', 'snakeBodyColor', 'percentOfItems'], function (items) {
+        snakeHead.value = items.snakeHeadColor;
+        snakeBody.value = items.snakeBodyColor;
+        percentText.value = items.percentOfItems;
+        init();
+    });
+
     startButton.addEventListener('click', function () {
         chrome.storage.sync.set({
             snakeBodyColor: snakeBody.value,
             snakeHeadColor: snakeHead.value,
             percentOfItems: parseInt(percentText.value)
         });
-        
-         chrome.tabs.executeScript(null, {"file" :  "snake.js"})
-            window.close();        
+
+        chrome.tabs.executeScript(null, { "file": "snake.js" })
+        window.close();
     });
-    document.getElementById("reset").onclick = function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
-            window.close();   
+    document.getElementById("reset").onclick = function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.update(tabs[0].id, { url: tabs[0].url });
+            window.close();
         });
 
     };
-    settingsButton.addEventListener('click', function(){ 
-          hiddenSettings.classList.toggle('hidden');   
-      });
+    settingsButton.addEventListener('click', function () {
+        hiddenSettings.classList.toggle('hidden');
+    });
 });
